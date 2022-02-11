2533e2f5833c3829e5069832ee811540ebdda6dc
Daniel Wan Rosli <danielwanrosli@DNa1ca81f.SUNet>
2022-02-10 23:14:06 -0800

    Revert "proj4"
    This reverts commit 925876d8c258b3aaa69872b7d4099e938106c511.

--- Song.js
+++ Song.js
@@ -4,57 +4,41 @@ import {
   Text,
   View,
   Image,
-  Pressable,
 } from 'react-native';
 import millisToMinutesAndSeconds from "./utils/millisToMinuteSeconds.js";
-import { AntDesign } from '@expo/vector-icons'; 
 
 
-export default function Song({ navigation, imageUrl, title, artist, album, duration, previewUrl, externalUrl }) {
+export default function Song({ index, imageUrl, title, artist, album, duration }) {
   return (
-    <Pressable
-    onPress={(e) => {
-        e.stopPropagation();
-        navigation.navigate("SongPreview", { url: externalUrl })
-    }}>
-        <View style={styles.container}>
-            {/* <View style={styles.overallContainer}> */}
-            <View style={styles.indexContainer}>
-                <Pressable
-                    onPress={(e) => {
-                        e.stopPropagation();
-                        navigation.navigate("SongPreview", { url: previewUrl })
-                    }}>
-                    <AntDesign name="play" size={24} color="rgb(29, 185, 84)" />   
-                </Pressable>
-            </View>
-            
-            <View style={styles.indexContainer}>
-                <Image source={{ uri: imageUrl }} style = {{ width: 50, height: 30}}/>
-            </View>
+    <View style={styles.container}>
+        {/* <View style={styles.overallContainer}> */}
+        <View style={styles.indexContainer}>
+             <Text style={{color: 'white'}}> {index}   </Text>
+         </View>
 
-            <View style={styles.indexContainer}>
-                <Text style={{color: 'white'}}> {title}    </Text>
-            </View>
+         <View style={styles.indexContainer}>
+             <Text style={{color: 'white'}}> {title}    </Text>
+         </View>
 
-            <View style={styles.indexContainer}>
-                <Text style={{color: 'white'}}> {artist}   </Text>
-            </View>
+         <View style={styles.indexContainer}>
+             <Text style={{color: 'white'}}> {artist}   </Text>
+         </View>
 
-            <View style={styles.indexContainer}>
-                <Text style={{color: 'white'}}> {album}   </Text>
-            </View>
+         <View style={styles.indexContainer}>
+             <Text style={{color: 'white'}}> {album}   </Text>
+         </View>
+
+         <View style={styles.indexContainer}>
+             <Image source={{ uri: imageUrl }} style = {{ width: 50, height: 30}}/>
+         </View>
 
 
+         <View style={styles.indexContainer}>
+             <Text style={{color: 'white'}}>{millisToMinutesAndSeconds(duration)}</Text>
+         </View>
 
-
-            <View style={styles.indexContainer}>
-                <Text style={{color: 'white'}}>{millisToMinutesAndSeconds(duration)}</Text>
-            </View>
-
-            {/* </View> */}
-        </View>
-    </Pressable>
+         {/* </View> */}
+     </View>
   );
 }
 
