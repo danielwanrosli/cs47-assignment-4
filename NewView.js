8fa91efc50d4a7725b65ee5500a0ef09ae600705
Daniel Wan Rosli <danielwanrosli@DNa1ca81f.SUNet>
2022-02-10 23:15:14 -0800

    Revert "proj4" commit
    This reverts commit 925876d8c258b3aaa69872b7d4099e938106c511.

--- NewView.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import { WebView } from "react-native-webview";
-
-export default function NewView({ route }) {
-    const url = route.params.url;
-    return (
-        <WebView source={{ uri: url }} />
-    );
-}
